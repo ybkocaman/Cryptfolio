ed09e6bf814b0e7f420209513db60a5975118f21
Yusuf <ybkocaman@gmail.com>
2024-04-27 17:48:26 +0100

    Initial Commit

---
+++ Cryptfolio/CryptfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptfolioApp.swift
+//  Cryptfolio
+//
+//  Created by Yusuf Burak on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
